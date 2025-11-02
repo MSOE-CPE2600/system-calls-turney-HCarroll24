1e15a75d5d6eeb6457fb5bb68275d7d1d2880289
Hunter Carroll <carrollh@msoe.edu>
2025-11-02 14:29:12 -0600

    Finished part 1

--- info.c
+++ info.c
@@ -10,23 +10,58 @@
  * compile: make clean, make
  * run: ./info
 *************************************************************************/
+#define _POSIX_C_SOURCE 199309L
+
 #include <time.h>
 #include <stdio.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
 
 int main(int argc, char* argv[])
 {
     // Current time of day in nanoseconds
-    struct timespec current;
+    struct timespec ts;
+    struct tm *local_time;
+    time_t seconds_since_midnight;
 
-    clock_gettime(CLOCK_REALTIME, &current);
+    clock_gettime(CLOCK_REALTIME, &ts);
+    local_time = localtime(&ts.tv_sec);
+    seconds_since_midnight = local_time->tm_hour*3600 + local_time->tm_min*60 + local_time->tm_sec;
+    printf("%-*s : %lu.%lu\n", 30, "1. Time of day", seconds_since_midnight, ts.tv_nsec);
 
-    // Conversion to day
-    time_t seconds = current.tv_sec;
-    struct tm *info = localtime(&seconds);
+    // Systems Network Name
+    struct utsname u;
+    if (uname(&u) == -1) {
+        perror("uname");
+        return 1;
+    }
+    printf("%-*s : %s\n", 30, "2. System Network Name", u.nodename);
 
-    char buffer[26];
-    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", info);
-    printf("Current local time: %s\n", buffer);
+    // Operating System's Name
+    printf("%-*s : %s\n", 30, "3. Operating System's Name", u.sysname);
+
+    // Operating System's Release and Version
+    printf("%-*s : %s\n", 30, "4. Operating System's Release", u.release);
+    printf("%-*s : %s\n", 30, "5. Operating System's Version", u.version);
+
+    // System's Hardware Type
+    printf("%-*s : %s\n", 30, "6. System's Hardware Type", u.machine);
+
+    // Number of CPUs
+    int num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    printf("%-*s : %d\n", 30, "7. Number of CPUs", num_cpus);
+
+    // Total Memory Physical (Bytes)
+    struct sysinfo info;
+    if (sysinfo(&info) == -1) {
+        perror("sysinfo");
+        return 1;
+    }
+    printf("%-*s : %lu bytes\n", 30, "8. Total Memory Physical", info.totalram * info.mem_unit);
+
+    // Total Amount of Free Memory (Bytes)
+    printf("%-*s : %lu bytes\n", 30, "9. Total Free Memory", info.freeram);
 
     return 0;
 }
--- pinfo.c
+++ pinfo.c
@@ -1,6 +1,20 @@
-// Name, etc
+/*************************************************************************
+ * @file pinfo.c
+ * @brief Write program that achieves system information tasks
+ *
+ * Name: Hunter Carroll
+ * Course: CPE 2600
+ * Section: 012
+ * Assignment: Lab 9
+ *
+ * compile: make clean, make
+ * run: ./pinfo
+*************************************************************************/
 
-// pinfo.c
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <sched.h>
 
 int main(int argc, char* argv[])
 {
